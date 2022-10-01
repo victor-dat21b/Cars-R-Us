80dcc603e7e7be21ebb3c8d409c4150f524575f7
Victor-Desktop <vict702k@stud.kea.dk>
2022-10-01 19:23:20 +0200

    Tried another thing to fix cors error

--- src/main/java/dat3/cars/configuration/CorsConfiguration.java
+++ src/main/java/dat3/cars/configuration/CorsConfiguration.java
@@ -11,7 +11,6 @@ public class CorsConfiguration implements WebMvcConfigurer
 {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST");
+        registry.addMapping("/**");
     }
 }
